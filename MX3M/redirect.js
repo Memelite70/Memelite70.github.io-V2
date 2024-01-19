e97a2b3b8c4a846a446ed76aabba414ba1e21d63
Memelite70 <memelite69@gmail.com>
2024-01-19 17:57:22 -0500

    Update and rename redirect.html to redirect.js

---
+++ MX3M/redirect.js
@@ -0,0 +1,9 @@
+if (window.location.href="https://splendorous-begonia-a301f1.netlify.app/mx3m/redirect") {
+  // block of code to be executed if condition1 is true
+window.location = "https://splendorous-begonia-a301f1.netlify.app"
+} else if (window.location.href="https://quicktest-geometry-tryhard.vercel.app/MX3M/redirect.html") {
+  // block of code to be executed if the condition1 is false and condition2 is true
+ window.location="https://quicktest-geometry-tryhard.vercel.app"
+} else {
+  // block of code to be executed if the condition1 is false and condition2 is false
+}

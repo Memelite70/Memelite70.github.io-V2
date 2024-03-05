e7b3e52864484ad970b4fcf834daed9fc7b35cd3
Memelite70 <memelite69@gmail.com>
2024-03-04 20:53:13 -0500

    Add files via upload

---
+++ Worksheets/Site/updates.js
@@ -0,0 +1,12 @@
+var updates = [
+    {message: "Welcome to the 3rd official release of Andyum!"}
+];
+var updatespage = document.getElementById("updatespage");
+for(let item of updates) {
+    let a = document.createElement("updatespage");
+    a.className = "updatespage";
+    var title = document.createElement("message");
+    title.textContent = item.message;
+    a.appendChild(title);
+    updatespage.appendChild(a);
+}

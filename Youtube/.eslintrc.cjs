fcb039fce326ee254c9db86baf44f8c18046070c
Memelite70 <memelite69@gmail.com>
2024-04-13 09:04:43 -0400

    Delete Youtube directory

--- Youtube/.eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    root: true,
-    env: {
-        node: true,
-    },
-    extends: ["plugin:vue/vue3-recommended", "eslint:recommended", "@unocss", "plugin:prettier/recommended"],
-};
